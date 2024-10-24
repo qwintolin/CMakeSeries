9f3b922d0e27da05f681ccddfeca61e6a654408c
Pruthviraj Patil <pruthviraj.patil@atlascopco.com>
2024-10-24 11:08:49 +0530

    Project1 code

---
+++ Project1/source/vector.cpp
@@ -0,0 +1,12 @@
+// C++ program to illustrate the
+// iterators in vector
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
